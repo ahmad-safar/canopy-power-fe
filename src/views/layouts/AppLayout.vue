98f6004ddde3d5314de566553f12772c0ff88f82
Ahmad Safar <ahmadsafar88@gmail.com>
2021-02-21 00:13:59 +0800

    Fix logo

--- src/views/layouts/AppLayout.vue
+++ src/views/layouts/AppLayout.vue
@@ -1,240 +1,105 @@
 <template>
   <div id="sidebar" class="c-sidebar c-sidebar-fixed c-sidebar-lg-show">
     <div class="c-sidebar-brand d-lg-down-none font-weight-bold">
-      <img src="/assets/img/canopy.svg" width="70%" />
+      <svg
+        style="enable-background:new 0 0 337.8 142.7;"
+        viewBox="0 0 337.8 142.7"
+        y="0px"
+        x="0px"
+        id="Layer_1"
+        version="1.1"
+      >
+        <title id="title5">Artboard 1</title>
+        <path
+          style="fill:#ffffff;fill-opacity:1"
+          id="path7"
+          d="M295.2,51.3C264,38,220.1,29.7,171.3,29.7S78.7,38,47.5,51.3c27.7-17.7,72.9-29.2,123.9-29.2  S267.5,33.6,295.2,51.3z"
+          class="st0"
+        />
+        <path
+          style="fill:#ffffff;fill-opacity:1"
+          id="path9"
+          d="M67,94.7c-2,1.2-4.1,2-6.3,2.5c-2.3,0.5-4.6,0.8-6.9,0.7c-2.9,0-5.7-0.4-8.4-1.3c-2.5-0.8-4.8-1.9-6.9-3.5  c-1.9-1.4-3.4-3.1-4.5-5.2c-2.2-3.9-2.2-8.7,0-12.6c1.2-2,2.7-3.8,4.6-5.2c2.1-1.5,4.4-2.7,6.9-3.4c2.7-0.9,5.5-1.3,8.4-1.3  c2.5,0,5,0.3,7.4,0.9c2,0.5,4,1.3,5.8,2.4l-3,2.8c-1.6-0.9-3.2-1.5-4.9-2c-1.7-0.4-3.4-0.6-5.1-0.6c-2.2,0-4.4,0.3-6.5,1  c-1.9,0.6-3.7,1.5-5.3,2.7c-1.4,1.1-2.6,2.4-3.5,4c-1.7,3.1-1.7,6.8,0,9.9c0.9,1.6,2.1,2.9,3.5,4c1.6,1.2,3.4,2.1,5.3,2.7  c2.1,0.7,4.3,1,6.5,1c1.7,0,3.4-0.2,5.1-0.6c1.7-0.5,3.4-1.1,5-1.9L67,94.7z"
+          class="st1"
+        />
+        <path
+          style="fill:#ffffff;fill-opacity:1"
+          id="path11"
+          d="M112.4,97.6l-6.2-10.2h-17L83,97.6h-5.6L97.7,65L118,97.6H112.4z M97.7,73.6L91.3,84h12.8L97.7,73.6z"
+          class="st1"
+        />
+        <path
+          style="fill:#ffffff;fill-opacity:1"
+          id="path13"
+          d="M134.9,74.5v23.1h-5.1V65l29.4,24.2V66h5.1v32.7L134.9,74.5z"
+          class="st1"
+        />
+        <path
+          style="fill:#ffffff;fill-opacity:1"
+          id="path15"
+          d="M222.8,81.8c0,2.2-0.6,4.4-1.7,6.3c-1.2,2-2.7,3.8-4.6,5.2c-2.1,1.5-4.4,2.7-6.9,3.5c-5.5,1.7-11.4,1.7-16.9,0  c-2.5-0.8-4.8-1.9-6.9-3.5c-1.9-1.4-3.5-3.1-4.6-5.2c-2.2-3.9-2.2-8.7,0-12.6c1.2-2,2.7-3.8,4.6-5.2c2.1-1.5,4.4-2.7,6.9-3.5  c5.5-1.7,11.4-1.7,16.9,0c2.5,0.8,4.8,1.9,6.9,3.5c1.9,1.4,3.5,3.1,4.6,5.2C222.2,77.4,222.8,79.6,222.8,81.8z M217.7,81.8  c0-1.7-0.4-3.5-1.3-5c-0.9-1.6-2.1-2.9-3.5-4c-1.6-1.2-3.4-2.1-5.3-2.7c-4.2-1.3-8.7-1.3-12.9,0c-1.9,0.6-3.7,1.5-5.3,2.7  c-1.5,1.1-2.7,2.4-3.5,4c-1.7,3.1-1.7,6.8,0,9.9c0.9,1.6,2.1,2.9,3.5,4c1.6,1.2,3.4,2.1,5.3,2.7c4.2,1.3,8.7,1.3,12.9,0  c1.9-0.6,3.7-1.5,5.3-2.7c1.5-1.1,2.7-2.4,3.5-4C217.3,85.2,217.7,83.6,217.7,81.8z"
+          class="st1"
+        />
+        <path
+          style="fill:#ffffff;fill-opacity:1"
+          id="path17"
+          d="M264.4,75.1c0,1.3-0.3,2.5-1,3.6c-0.7,1.1-1.6,2.1-2.7,2.9c-1.3,0.9-2.6,1.5-4.1,1.9c-1.7,0.5-3.5,0.7-5.3,0.7  H243v13.4h-5V66h13.4c1.8,0,3.5,0.2,5.3,0.7c1.5,0.4,2.9,1,4.1,1.9c1.1,0.8,2,1.7,2.7,2.9C264.1,72.6,264.4,73.9,264.4,75.1z   M259.3,75.1c0-0.8-0.2-1.5-0.5-2.2c-0.4-0.7-0.9-1.3-1.6-1.8c-0.8-0.6-1.6-1-2.5-1.3c-1.1-0.3-2.2-0.5-3.4-0.5H243v11.4h8.2  c1.1,0,2.3-0.1,3.4-0.4c0.9-0.3,1.8-0.7,2.5-1.2c0.7-0.5,1.2-1.1,1.6-1.8C259.1,76.6,259.3,75.9,259.3,75.1L259.3,75.1z"
+          class="st1"
+        />
+        <path
+          style="fill:#ffffff;fill-opacity:1"
+          id="path19"
+          d="M294.8,83.6v14h-5.1v-14L273.8,66h6.1l12.3,13.4L304.5,66h6.1L294.8,83.6z"
+          class="st1"
+        />
+        <path
+          style="fill:#ffffff;fill-opacity:1"
+          id="path21"
+          d="M247,107.9c0,0.5-0.1,0.9-0.3,1.4c-0.2,0.4-0.5,0.8-0.9,1.1c-0.4,0.3-0.9,0.6-1.4,0.7c-0.6,0.2-1.2,0.3-1.8,0.3  h-2.9v5.1h-1.7v-11.9h4.6c0.6,0,1.2,0.1,1.8,0.3c0.5,0.2,1,0.4,1.4,0.7c0.4,0.3,0.7,0.7,0.9,1.1C246.9,107,247,107.4,247,107.9z   M245.2,107.9c0-0.3-0.1-0.6-0.2-0.8c-0.1-0.3-0.3-0.5-0.6-0.7c-0.3-0.2-0.6-0.4-0.9-0.5c-0.4-0.1-0.8-0.2-1.2-0.2h-2.8v4.3h2.8  c0.4,0,0.8-0.1,1.2-0.2c0.3-0.1,0.6-0.3,0.9-0.5c0.2-0.2,0.4-0.4,0.6-0.7C245.2,108.4,245.2,108.2,245.2,107.9L245.2,107.9z"
+          class="st2"
+        />
+        <path
+          style="fill:#ffffff;fill-opacity:1"
+          id="path23"
+          d="M264.1,110.4c0,0.8-0.2,1.6-0.6,2.4c-0.4,0.7-0.9,1.4-1.6,1.9c-0.7,0.6-1.5,1-2.4,1.3c-1.9,0.6-3.9,0.6-5.8,0  c-0.9-0.3-1.7-0.7-2.4-1.3c-0.7-0.5-1.2-1.2-1.6-1.9c-0.8-1.5-0.8-3.3,0-4.8c0.4-0.7,0.9-1.4,1.6-1.9c0.7-0.6,1.5-1,2.4-1.3  c1.9-0.6,3.9-0.6,5.8,0c0.9,0.3,1.7,0.7,2.4,1.3c0.7,0.5,1.2,1.2,1.6,1.9C263.9,108.8,264.1,109.6,264.1,110.4z M262.3,110.4  c0-0.6-0.1-1.3-0.4-1.9c-0.3-0.6-0.7-1.1-1.2-1.5c-0.5-0.4-1.2-0.8-1.8-1c-1.4-0.5-3-0.5-4.4,0c-0.7,0.2-1.3,0.6-1.8,1  c-0.5,0.4-0.9,0.9-1.2,1.5c-0.6,1.2-0.6,2.6,0,3.7c0.3,0.6,0.7,1.1,1.2,1.5c0.5,0.4,1.2,0.8,1.8,1c1.4,0.5,3,0.5,4.4,0  c0.7-0.2,1.3-0.6,1.8-1c0.5-0.4,0.9-0.9,1.2-1.5C262.2,111.7,262.3,111.1,262.3,110.4z"
+          class="st2"
+        />
+        <path
+          style="fill:#ffffff;fill-opacity:1"
+          id="path25"
+          d="M279.4,116.8l-4.4-9l-4.4,9l-5-12.3h1.8l3.3,8.3l4.2-8.7l4.2,8.7l3.3-8.3h1.8L279.4,116.8z"
+          class="st2"
+        />
+        <path
+          style="fill:#ffffff;fill-opacity:1"
+          id="path27"
+          d="M287.1,116.4v-11.9h8.5v1.3h-6.8v4h6.5v1.3h-6.5v4.1h6.8v1.3H287.1z"
+          class="st2"
+        />
+        <path
+          style="fill:#ffffff;fill-opacity:1"
+          id="path29"
+          d="M308.2,116.4l-6.3-6.2h2.2c0.8,0,1.6-0.2,2.2-0.6c0.9-0.6,1.1-1.9,0.5-2.8c-0.1-0.2-0.3-0.3-0.5-0.5  c-0.7-0.4-1.4-0.6-2.2-0.6h-2.8v10.7h-1.8v-11.9h4.7c0.7,0,1.4,0.1,2,0.3c0.5,0.2,1,0.4,1.5,0.7c0.4,0.3,0.7,0.7,0.9,1.1  c0.2,0.4,0.3,0.9,0.3,1.4c0,0.4-0.1,0.9-0.3,1.3c-0.2,0.4-0.5,0.7-0.8,1c-0.4,0.3-0.8,0.5-1.2,0.7c-0.5,0.2-1,0.3-1.5,0.3l5.3,5.1  L308.2,116.4z"
+          class="st2"
+        />
+        <path
+          style="fill:#ffffff;fill-opacity:1"
+          id="path31"
+          d="M295.2,51.3C295.2,51.3,295.2,51.3,295.2,51.3C264,38,220,29.7,171.3,29.7c-48.7,0-92.7,8.3-123.9,21.6  c-6,2.5-11.8,5.4-17.4,8.7c3.2-1.2,6.4-2.3,9.8-3.4c34.8-11.2,80.9-18,131.4-18c50.6,0,96.7,6.8,131.4,18c3.3,1.1,6.7,2.3,9.8,3.4  C307,56.8,301.2,53.9,295.2,51.3z"
+          class="st2"
+        />
+      </svg>
     </div>
     <ul class="c-sidebar-nav">
       <li class="c-sidebar-nav-item">
-        <a class="c-sidebar-nav-link" href="index.html">
+        <router-link class="c-sidebar-nav-link" to="/">
           <i class="c-sidebar-nav-icon cil-speedometer" /> Dashboard
-          <span class="badge badge-info">NEW</span>
-        </a>
-      </li>
-      <li class="c-sidebar-nav-title">Theme</li>
-      <li class="c-sidebar-nav-item">
-        <a class="c-sidebar-nav-link" href="colors.html">
-          <i class="c-sidebar-nav-icon cil-drop" /> Colors
-        </a>
+        </router-link>
       </li>
       <li class="c-sidebar-nav-item">
-        <a class="c-sidebar-nav-link" href="typography.html">
-          <i class="c-sidebar-nav-icon cil-pencil" /> Typography
-        </a>
-      </li>
-      <li class="c-sidebar-nav-title">Components</li>
-      <li class="c-sidebar-nav-item c-sidebar-nav-dropdown">
-        <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-          <i class="c-sidebar-nav-icon cil-puzzle" /> Base
-        </a>
-        <ul class="c-sidebar-nav-dropdown-items">
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="base/breadcrumb.html">
-              <span class="c-sidebar-nav-icon" /> Breadcrumb
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="base/cards.html">
-              <span class="c-sidebar-nav-icon" /> Cards
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="base/carousel.html">
-              <span class="c-sidebar-nav-icon" /> Carousel
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="base/collapse.html">
-              <span class="c-sidebar-nav-icon" /> Collapse
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="base/forms.html">
-              <span class="c-sidebar-nav-icon" /> Forms
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="base/jumbotron.html">
-              <span class="c-sidebar-nav-icon" /> Jumbotron
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="base/list-group.html">
-              <span class="c-sidebar-nav-icon" /> List group
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="base/navs.html">
-              <span class="c-sidebar-nav-icon" /> Navs
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="base/pagination.html">
-              <span class="c-sidebar-nav-icon" /> Pagination
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="base/popovers.html">
-              <span class="c-sidebar-nav-icon" /> Popovers
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="base/progress.html">
-              <span class="c-sidebar-nav-icon" /> Progress
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="base/scrollspy.html">
-              <span class="c-sidebar-nav-icon" /> Scrollspy
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="base/switches.html">
-              <span class="c-sidebar-nav-icon" /> Switches
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="base/tables.html">
-              <span class="c-sidebar-nav-icon" /> Tables
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="base/tabs.html">
-              <span class="c-sidebar-nav-icon" /> Tabs
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="base/tooltips.html">
-              <span class="c-sidebar-nav-icon" /> Tooltips
-            </a>
-          </li>
-        </ul>
-      </li>
-      <li class="c-sidebar-nav-item c-sidebar-nav-dropdown">
-        <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-          <i class="c-sidebar-nav-icon cil-cursor" /> Buttons
-        </a>
-        <ul class="c-sidebar-nav-dropdown-items">
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="buttons/buttons.html">
-              <span class="c-sidebar-nav-icon" /> Buttons
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="buttons/button-group.html">
-              <span class="c-sidebar-nav-icon" /> Buttons Group
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="buttons/dropdowns.html">
-              <span class="c-sidebar-nav-icon" /> Dropdowns
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="buttons/brand-buttons.html">
-              <span class="c-sidebar-nav-icon" /> Brand Buttons
-            </a>
-          </li>
-        </ul>
-      </li>
-      <li class="c-sidebar-nav-item">
-        <a class="c-sidebar-nav-link" href="charts.html">
-          <i class="c-sidebar-nav-icon cil-chart" /> Charts
-        </a>
-      </li>
-      <li class="c-sidebar-nav-dropdown">
-        <a class="c-sidebar-nav-dropdown-toggle" href="#">
-          <i class="c-sidebar-nav-icon cil-star" /> Icons
-        </a>
-        <ul class="c-sidebar-nav-dropdown-items">
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="icons/coreui-icons-free.html">
-              CoreUI Icons
-              <span class="badge badge-success">Free</span>
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="icons/coreui-icons-brand.html">CoreUI Icons - Brand</a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="icons/coreui-icons-flag.html">CoreUI Icons - Flag</a>
-          </li>
-        </ul>
-      </li>
-      <li class="c-sidebar-nav-item c-sidebar-nav-dropdown">
-        <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-          <i class="c-sidebar-nav-icon cil-bell" /> Notifications
-        </a>
-        <ul class="c-sidebar-nav-dropdown-items">
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="notifications/alerts.html">
-              <span class="c-sidebar-nav-icon" /> Alerts
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="notifications/badge.html">
-              <span class="c-sidebar-nav-icon" /> Badge
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="notifications/modals.html">
-              <span class="c-sidebar-nav-icon" /> Modals
-            </a>
-          </li>
-        </ul>
-      </li>
-      <li class="c-sidebar-nav-item">
-        <a class="c-sidebar-nav-link" href="widgets.html">
-          <i class="c-sidebar-nav-icon cil-calculator" /> Widgets
-          <span class="badge badge-info">NEW</span>
-        </a>
-      </li>
-      <li class="c-sidebar-nav-divider" />
-      <li class="c-sidebar-nav-title">Extras</li>
-      <li class="c-sidebar-nav-item c-sidebar-nav-dropdown">
-        <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-          <i class="c-sidebar-nav-icon cil-star" /> Pages
-        </a>
-        <ul class="c-sidebar-nav-dropdown-items">
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="login.html" target="_top">
-              <i class="c-sidebar-nav-icon cil-account" /> Login
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="register.html" target="_top">
-              <i class="c-sidebar-nav-icon cil-account" /> Register
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="404.html" target="_top">
-              <i class="c-sidebar-nav-icon cil-bug" /> Error 404
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="500.html" target="_top">
-              <i class="c-sidebar-nav-icon cil-bug" /> Error 500
-            </a>
-          </li>
-        </ul>
-      </li>
-      <li class="c-sidebar-nav-item mt-auto">
-        <a
-          class="c-sidebar-nav-link c-sidebar-nav-link-success"
-          href="https://coreui.io"
-          target="_top"
-        >
-          <i class="c-sidebar-nav-icon cil-cloud" /> Download CoreUI
-        </a>
-      </li>
-      <li class="c-sidebar-nav-item">
-        <a
-          class="c-sidebar-nav-link c-sidebar-nav-link-danger"
-          href="https://coreui.io/pro/"
-          target="_top"
-        >
-          <i class="c-sidebar-nav-icon cil-layers" /> Try CoreUI
-          <strong>PRO</strong>
-        </a>
+        <router-link class="c-sidebar-nav-link" to="/incident">
+          <i class="c-sidebar-nav-icon cil-book" /> Incident Overview
+        </router-link>
       </li>
     </ul>
     <!-- <button
@@ -257,7 +122,7 @@
         </i>
       </button>
       <a class="c-header-brand d-lg-none" href="#">
-        <div class="c-sidebar-brand-full font-weight-bold">Canopy</div>
+        <div class="c-sidebar-brand-full font-weight-bold">Canopy Power</div>
       </a>
       <button
         class="c-header-toggler c-class-toggler mfs-3 d-md-down-none"
@@ -302,15 +167,6 @@
           </div>
         </li>
       </ul>
-      <div class="c-subheader px-3">
-        <ol class="breadcrumb border-0 m-0">
-          <li class="breadcrumb-item">Home</li>
-          <li class="breadcrumb-item">
-            <a href="#">Admin</a>
-          </li>
-          <li class="breadcrumb-item active">Dashboard</li>
-        </ol>
-      </div>
     </header>
     <div class="c-body">
       <main class="c-main">
@@ -335,10 +191,10 @@ export default {
     const router = useRouter();
 
     const logout = () => {
-      localStorage.removeItem("user");
-      router.replace("/");
+      localStorage.removeItem('user')
+      router.replace('/login')
     };
-    return { logout };
+    return { logout }
   },
 };
 </script>
